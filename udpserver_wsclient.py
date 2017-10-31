622981a5c3dbbbcb01a0e172403ceb69866fb3c4
takuma@kuma <t-sato@trondule.co.jp>
2017-10-31 10:59:44 +0900

    rename

---
+++ udpserver_wsclient.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+from websocket import create_connection
+
+# socket; Low-level networking interface
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('', 39152))
+
+# websocket
+ws = create_connection('ws://localhost:3000/websocket')
+
+while True:
+    try:
+        data = s.recv(1024)
+    except socket.error:
+        pass
+    else:
+        ws.send(data)
